fa95c81b7d6f0c802f41ea7cf7b1d1b0da0909f2
VARUN-017 <kbvarun43@gmail.com>
2025-09-02 17:16:27 +0530

    kafka

---
+++ src/main/java/com/Aimers/journalApp/model/SentimentData.java
@@ -0,0 +1,14 @@
+package com.Aimers.journalApp.model;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SentimentData {
+
+    private String email;
+    private String sentiment;
+
+}
