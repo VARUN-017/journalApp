dc5c2e2a1306de584e8fd1fe28a7263eb7da98a3
VARUN-017 <kbvarun43@gmail.com>
2025-09-04 23:33:18 +0530

    minor changes

--- src/test/java/com/Aimers/journalApp/JournalAppApplicationTests.java
+++ src/test/java/com/Aimers/journalApp/JournalAppApplicationTests.java
@@ -1,17 +1,32 @@
-/*
 package com.Aimers.journalApp;
 
-import org.junit.jupiter.api.Disabled;
+
+import com.Aimers.journalApp.entity.User;
+import com.Aimers.journalApp.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class JournalAppApplicationTests {
 
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
-	@Disabled
 	void contextLoads() {
+		assertEquals(5,2+3);
+		User user = userRepository.findByUserName("SteveRogers");
+        assertFalse(user.getJournalEntries().isEmpty());
 	}
 
 }
-*/
