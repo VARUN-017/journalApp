43f9ab597f5eadb45977c19ac08236bd64a9f9e2
VARUN-017 <kbvarun43@gmail.com>
2025-09-04 23:26:06 +0530

    bean creation

--- src/main/java/com/Aimers/journalApp/config/TransactionConfig.java
+++ src/main/java/com/Aimers/journalApp/config/TransactionConfig.java
@@ -4,15 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableTransactionManagement
+@EnableScheduling
 public class TransactionConfig {
 
     @Bean
     public PlatformTransactionManager manager(MongoDatabaseFactory dbFactory){
         return new MongoTransactionManager(dbFactory);
     }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
+
 }
