99d2629bfd12722dc018d5b4ca320df6c834b426
VARUN-017 <kbvarun43@gmail.com>
2025-09-04 23:34:31 +0530

    db related tests

--- src/test/java/com/Aimers/journalApp/service/UserDetailsServiceImplTests.java
+++ src/test/java/com/Aimers/journalApp/service/UserDetailsServiceImplTests.java
@@ -1,54 +1,43 @@
-/*
 package com.Aimers.journalApp.service;
 
 import com.Aimers.journalApp.entity.User;
 import com.Aimers.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.*;
+
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
-
+@ActiveProfiles("dev")
 public class UserDetailsServiceImplTests {
 
-    @Autowired
+
+    @InjectMocks
     private UserDetailsServiceImpl userDetailsService;
 
-    @MockBean
+    @Mock
     private UserRepository userRepository;
 
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+
     @Test
-    @Disabled
     void loadUserByUsernameTest(){
-        when(userRepository.findByUserName("varun")).thenReturn(User.builder().userName("varun").password("ram").roles(new ArrayList<>()).build());
-        UserDetails user = userDetailsService.loadUserByUsername("varun");
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("inrinrick").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsService.loadUserByUsername("SteveRogers");
         Assertions.assertNotNull(user);
-   }
+    }
 }
 
-
-// Pure Mockito mock without loading spring context
-   */
-/* @ExtendWith(MockitoExtension.class)
-    public class UserDetailsServiceImplTests {
-
-        @InjectMocks
-        private UserDetailsServiceImpl userDetailsService;
-
-        @Mock
-        private UserRepository userRepository;
-
-        @Test
-        void loadUserByUsernameTest(){
-            when(userRepository.findByUserName("varun")).thenReturn(User.builder().userName("varun").password("ram").roles(new ArrayList<>()).build());
-            UserDetails user = userDetailsService.loadUserByUsername("varun");
-            Assertions.assertNotNull(user);
-        }*/
