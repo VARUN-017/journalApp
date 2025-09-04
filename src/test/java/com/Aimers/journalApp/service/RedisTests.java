248c322d3a7f81a735998329be3d08fde01cee37
VARUN-017 <kbvarun43@gmail.com>
2025-09-05 00:08:08 +0530

    testing redis locally

---
+++ src/test/java/com/Aimers/journalApp/service/RedisTests.java
@@ -0,0 +1,25 @@
+package com.Aimers.journalApp.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+
+    @Test
+    @Disabled
+    void testSendEmail(){
+        redisTemplate.opsForValue().set("email","varun@email.com");
+        Object salary = redisTemplate.opsForValue().get("salary");
+        int a = 10;
+    }
+}
