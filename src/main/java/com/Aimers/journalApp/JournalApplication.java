60787054359d014dcada08f8b11f653bb0f0dfe8
VARUN-017 <kbvarun43@gmail.com>
2025-09-09 22:21:10 +0530

    to kafka disable logs on render

--- src/main/java/com/Aimers/journalApp/JournalApplication.java
+++ src/main/java/com/Aimers/journalApp/JournalApplication.java
@@ -3,10 +3,14 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 
-@SpringBootApplication
+
+@SpringBootApplication(
+		exclude = {KafkaAutoConfiguration.class}
+)
 public class JournalApplication {
 
 	public static void main(String[] args) {
