d28f4bae2522d8e93c115898da7112840573fa12
VARUN-017 <kbvarun43@gmail.com>
2025-09-09 22:02:07 +0530

    to avoid kafka logs in render

---
+++ src/main/java/com/Aimers/journalApp/config/KafkaConfigToggle.java
@@ -0,0 +1,20 @@
+package com.Aimers.journalApp.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@ConditionalOnProperty(
+        value = "kafka.enabled",
+        havingValue = "true",
+        matchIfMissing = true
+)
+@Import(KafkaAutoConfiguration.class)
+@EnableKafka
+public class KafkaConfigToggle {
+
+
+}
