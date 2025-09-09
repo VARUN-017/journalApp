d16754df882cd25cb1af0faefcb20da8e1ba8088
VARUN-017 <kbvarun43@gmail.com>
2025-09-09 22:40:49 +0530

    minor changes for kafka

--- src/main/java/com/Aimers/journalApp/service/SentimentProducerService.java
+++ src/main/java/com/Aimers/journalApp/service/SentimentProducerService.java
@@ -2,10 +2,12 @@ package com.Aimers.journalApp.service;
 
 import com.Aimers.journalApp.model.SentimentData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty(name = "kafka.enabled", havingValue = "true")
 public class SentimentProducerService {
 
     @Autowired
