f7492b5bdea784eb48fa6123c04d59e9eca8a3e6
VARUN-017 <kbvarun43@gmail.com>
2025-09-09 23:18:14 +0530

    main app

--- src/main/java/com/Aimers/journalApp/JournalApplication.java
+++ src/main/java/com/Aimers/journalApp/JournalApplication.java
@@ -3,14 +3,10 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 
-
-@SpringBootApplication(
-		exclude = {KafkaAutoConfiguration.class}
-)
+@SpringBootApplication
 public class JournalApplication {
 
 	public static void main(String[] args) {
